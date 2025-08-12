29761b6315639643f9dd09da28ba7c66086139e6
Ylella <genshinylella@gmail.com>
2025-08-12 08:38:30 +0200

    Update register-command.js

--- register-command.js
+++ register-command.js
@@ -1,32 +1,37 @@
 import fetch from 'node-fetch';
 
-const APP_ID = 'TU_APP_ID';
-const GUILD_ID = 'TU_GUILD_ID';
-const BOT_TOKEN = 'TU_BOT_TOKEN';
+const APP_ID = process.env.APP_ID;
+const GUILD_ID = process.env.GUILD_ID;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const command = {
+const COMMAND = {
   name: 'buscar',
-  description: 'Busca un nombre en la hoja',
+  description: 'Busca un nombre en la hoja de Google Sheets',
+  type: 1,
   options: [
     {
       name: 'nombre',
       description: 'Nombre a buscar',
-      type: 3, // STRING
+      type: 3,
       required: true
     }
   ]
 };
 
-const url = `https://discord.com/api/v10/applications/${APP_ID}/guilds/${GUILD_ID}/commands`;
+async function registerCommand() {
+  const url = `https://discord.com/api/v10/applications/${APP_ID}/guilds/${GUILD_ID}/commands`;
 
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Authorization': `Bot ${BOT_TOKEN}`,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(command)
-})
-  .then(res => res.json())
-  .then(console.log)
-  .catch(console.error);
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bot ${BOT_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(COMMAND)
+  });
+
+  const data = await response.json();
+  console.log('Comando registrado:', data);
+}
+
+registerCommand().catch(console.error);
